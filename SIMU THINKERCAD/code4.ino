22b300e4eaf389977e99e0584c1b9a3d1c4644ed
siphou <siphou14@gmail.com>
2025-10-07 11:32:01 +0200

    Update code4.ino

--- SIMU THINKERCAD/code4.ino
+++ SIMU THINKERCAD/code4.ino
@@ -1,4 +1,3 @@
-
 #define PIN_LED_ROUGE 9
 #define PIN_LED_VERTE 10
 #define PIN_LED_BLEUE 11
@@ -6,7 +5,6 @@
 #define POT_ROUGE A0
 #define POT_VERT  A1
 #define POT_BLEU  A2
-
 
 const bool LED_ANODE_COMMUNE = false;
 
@@ -21,18 +19,15 @@ void setup() {
 
   Serial.begin(9600);
 }
-
 void loop() {
  
   int nR = analogRead(POT_ROUGE);
   int nV = analogRead(POT_VERT);
   int nB = analogRead(POT_BLEU);
 
-
   uint8_t r = nR >> 2;
   uint8_t g = nV >> 2;
   uint8_t b = nB >> 2;
-
 
   if (LED_ANODE_COMMUNE) {
     r = 255 - r;
@@ -40,11 +35,9 @@ void loop() {
     b = 255 - b;
   }
 
-
   analogWrite(PIN_LED_ROUGE, r);
   analogWrite(PIN_LED_VERTE, g);
   analogWrite(PIN_LED_BLEUE, b);
-
 
   Serial.print("R=");
   Serial.print(r);   Serial.print(" (");
