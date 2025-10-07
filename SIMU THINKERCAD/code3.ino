83cff12e34764c9f3dd115021cc8e8f1f1acde94
siphou <siphou14@gmail.com>
2025-10-07 11:08:48 +0200

    Update code3.ino

--- SIMU THINKERCAD/code3.ino
+++ SIMU THINKERCAD/code3.ino
@@ -1,34 +1,24 @@
+#define INTERRUPTEUR_ROUGE A0
+#define INTERRUPTEUR_VERT A1
+#define INTERRUPTEUR_BLEU A2
 
 #define PIN_LED_ROUGE 9
-#define PIN_LED_VERTE 10
-#define PIN_LED_BLEUE 11
-
-#define INTERRUPTEUR_ROUGE A0
-#define INTERRUPTEUR_VERT A2
-#define INTERRUPTEUR_BLEU A3
+#define PIN_LED_VERT 10
+#define PIN_LED_BLEU 11
 
 void setup() {
-
-  pinMode(PIN_LED_ROUGE, OUTPUT);
-  pinMode(PIN_LED_VERTE, OUTPUT);
-  pinMode(PIN_LED_BLEUE, OUTPUT);
-
  
   pinMode(INTERRUPTEUR_ROUGE, INPUT);
   pinMode(INTERRUPTEUR_VERT, INPUT);
   pinMode(INTERRUPTEUR_BLEU, INPUT);
+
+  pinMode(PIN_LED_ROUGE, OUTPUT);
+  pinMode(PIN_LED_VERT, OUTPUT);
+  pinMode(PIN_LED_BLEU, OUTPUT);
 }
 
 void loop() {
-
-  int etatRouge = digitalRead(INTERRUPTEUR_ROUGE);
-  int etatVert = digitalRead(INTERRUPTEUR_VERT);
-  int etatBleu = digitalRead(INTERRUPTEUR_BLEU);
-
-
-  analogWrite(PIN_LED_ROUGE, 255 * etatRouge);
-  analogWrite(PIN_LED_VERTE, 255 * etatVert);
-  analogWrite(PIN_LED_BLEUE, 255 * etatBleu);
-
-  delay(20); 
+  analogWrite(PIN_LED_ROUGE, 255 * digitalRead(INTERRUPTEUR_ROUGE));
+  analogWrite(PIN_LED_VERT, 255 * digitalRead(INTERRUPTEUR_VERT));
+  analogWrite(PIN_LED_BLEU, 255 * digitalRead(INTERRUPTEUR_BLEU));
 }
